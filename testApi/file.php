8961803737bf4e241e0a8e77a2147da0cbb54bc9
pravin <bhosale.pravinp@gmail.com>
2019-08-03 15:51:17 +0530

    test api

---
+++ testApi/file.php
@@ -0,0 +1,21 @@
+<?php
+  header('Access-Control-Allow-Origin:*');
+  header('Content-Type:application/json');
+  sleep(10);
+ ?>
+
+{
+  "result":[{
+      "name": "pravin",
+      "online": true
+    }, {
+      "name": "abc",
+      "online": false
+    }, {
+      "name": "xyz",
+      "online": true
+    }, {
+      "name": "qwe",
+      "online": false
+    }]
+  }
