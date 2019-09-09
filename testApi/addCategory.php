37ce6c65d8c260f449ff7873ccf77546cbb5f96b
pravin-winsoft <36952580+pravin-winsoft@users.noreply.github.com>
2019-09-09 18:55:56 +0530

    add category API changes

--- testApi/addCategory.php
+++ testApi/addCategory.php
@@ -1,18 +1,34 @@
 <?php
     require 'common.php';
-    
+
     $data = json_decode(file_get_contents("php://input"));
 
-    $cname = $data->cname;
-    $type = $data->type;
+    // $cname = $data->cname;
+    // $type = $data->type;
+
+    // $temp = [
+    //     {cname: "1", type: "i"},
+    //     {cname: "2", type: "2"},
+    //     {cname: "3", type: "i"},
+    // ];
+    //
+    // print_r($temp);
+
+    echo(gettype($data));
+
+    $str = "";
+
+    foreach ($data as $key => $value) {
+        echo($value->cname.' '.$value->type);
+    }
 
     $res = '{
         "message":"Categories added successfuly",
         "success": true,
         "response": [
-          {"cname":"'.$cname.'","type":"'.$type.'"}
+          {"cname":"","type":""}
         ]
       }';
   sleep(2);
-  echo($res);
+  echo($str);
  ?>
