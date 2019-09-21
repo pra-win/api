682f30db578cb282d238c805360f996aeb47ceb7
pra-win <bhosale.pravinp@gmail.com>
2019-09-21 19:58:12 +0530

    f key added

--- testApi/SQL/money-manager.sql
+++ testApi/SQL/money-manager.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 21, 2019 at 04:37 PM
+-- Generation Time: Sep 21, 2019 at 07:56 PM
 -- Server version: 5.7.27-0ubuntu0.16.04.1
 -- PHP Version: 7.0.33-0ubuntu0.16.04.6
 
@@ -33,21 +33,6 @@ CREATE TABLE `category-master` (
   `category-added-date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `category-master`
---
-
-INSERT INTO `category-master` (`category-id`, `category-name`, `category-type`, `category-added-date`) VALUES
-(13, 'dsd', 'i', '2019-09-21 03:01:09'),
-(14, 'dsd-2', 'e', '2019-09-21 03:01:48'),
-(15, 'dsd-3', 'i', '2019-09-21 03:02:11'),
-(16, 'dsd-4', 'e', '2019-09-21 03:02:11'),
-(20, 'dsd-11', 'e', '2019-09-21 03:12:01'),
-(21, 'dsd-7895', 'i', '2019-09-21 03:30:00'),
-(22, 'tedt', 'e', '2019-09-21 03:30:00'),
-(23, 'test-3', 'i', '2019-09-21 04:06:04'),
-(24, 'test-4', 'e', '2019-09-21 04:06:15');
-
 -- --------------------------------------------------------
 
 --
@@ -64,13 +49,6 @@ CREATE TABLE `transaction` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `transaction`
---
-
-INSERT INTO `transaction` (`transaction-id`, `transaction-amt`, `category-id`, `transaction-date`, `transaction-desc`, `transaction-keyword`) VALUES
-(1, 999999, 1, '2019-09-21 00:00:00', 'test t', 'test t k');
-
---
 -- Indexes for dumped tables
 --
 
@@ -85,7 +63,8 @@ ALTER TABLE `category-master`
 -- Indexes for table `transaction`
 --
 ALTER TABLE `transaction`
-  ADD PRIMARY KEY (`transaction-id`);
+  ADD PRIMARY KEY (`transaction-id`),
+  ADD KEY `category-id` (`category-id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -95,12 +74,22 @@ ALTER TABLE `transaction`
 -- AUTO_INCREMENT for table `category-master`
 --
 ALTER TABLE `category-master`
-  MODIFY `category-id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `category-id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 --
 -- AUTO_INCREMENT for table `transaction`
 --
 ALTER TABLE `transaction`
-  MODIFY `transaction-id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `transaction-id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`category-id`) REFERENCES `category-master` (`category-id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
