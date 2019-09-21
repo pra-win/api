e4916a4c34fa4f4f7f76535775ba1ab1081b355b
pra-win <bhosale.pravinp@gmail.com>
2019-09-21 13:39:15 +0530

    file upload cleanup

--- testApi/fileUploadTest.php
+++ testApi/fileUploadTest.php
@@ -22,17 +22,17 @@ $response = array();
 $upload_dir = '/home/pravin/www/html/api/testApi/uploads/';
 $server_url = 'http://localhost:80';
 
-$data = file_get_contents("php://input", false);
+//$data = file_get_contents("php://input", false);
 //print_r($data);
-echo ('test'.file_put_contents("outputfile.txt", "test"));
+//echo ('test'.file_put_contents("outputfile.txt", "test"));
 //
 // exit;
 
-if($_FILES['avatar'])
+if($_FILES['file'])
 {
-    $avatar_name = $_FILES["avatar"]["name"];
-    $avatar_tmp_name = $_FILES["avatar"]["tmp_name"];
-    $error = $_FILES["avatar"]["error"];
+    $file_name = $_FILES["file"]["name"];
+    $file_tmp_name = $_FILES["file"]["tmp_name"];
+    $error = $_FILES["file"]["error"];
     if($error > 0){
         $response = array(
             "status" => "error",
@@ -41,11 +41,11 @@ if($_FILES['avatar'])
         );
     }else
     {
-        $random_name = rand(1000,1000000)."-".$avatar_name;
+        $random_name = rand(1000,1000000)."-".$file_name;
         $upload_name = $upload_dir.strtolower($random_name);
         $upload_name = preg_replace('/\s+/', '-', $upload_name);
 
-        if(move_uploaded_file($avatar_tmp_name , $upload_name)) {
+        if(move_uploaded_file($file_tmp_name , $upload_name)) {
             $response = array(
                 "status" => "success",
                 "error" => false,
